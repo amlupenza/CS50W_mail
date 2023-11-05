dff35e19f4329ac69c4f2bf46ffdfd1427787ade
amlupenza <amlupenza@gmail.com>
2023-11-05 11:15:22 +0300

    archive work but click event apply to both parent and child

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,19 +1,49 @@
+
 document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('popstate', function(event){
+    if(event.state){
+      if(event.state.email){
+        view_email(event.state.email);
+      }
+      else if(event.state.mailbox){
+        load_mailbox(event.state.mailbox);
+      }
+    }
+    else{
+      load_mailbox('inbox');
+    }
+  })
 
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#inbox').addEventListener('click', () => {
+    history.pushState({mailbox: 'inbox'}, '', 'inbox');
+    load_mailbox('inbox');
+  });
+  document.querySelector('#sent').addEventListener('click', () => {
+    history.pushState({mailbox: 'sent'}, '', 'sent');
+    load_mailbox('sent');
+  });
+  document.querySelector('#archived').addEventListener('click', () => {
+    history.pushState({mailbox: 'archive'}, '', 'archive');
+    load_mailbox('archive');
+  });
+  document.querySelector('#compose').addEventListener('click', () => {
+    history.pushState({compose: 'compose'}, '', 'compose');
+    compose_email();
+  });
+  
 
   // By default, load the inbox
   load_mailbox('inbox');
 })
+let current_mailbox = 'inbox';
+
 
 function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -42,9 +72,13 @@ function compose_email() {
 
 
 function load_mailbox(mailbox) {
+  current_mailbox = mailbox;
+  document.querySelector('#emails-view').innerHTML = '';
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
+  current_mailbox = mailbox;
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   // get emails
@@ -69,13 +103,8 @@ function load_mailbox(mailbox) {
         })
         .then(response => {
           if (response.status === 204){
-            //hide emails-view pages
-            document.querySelector('#emails-view').style.display = 'none'
-            //view an email
-            const element = document.createElement('div');
-            element.innerHTML = `<h6>From: ${emails[i].sender}</6><h6>Subject: ${emails[i].subject}</6><p id>${emails[i].timestamp}</p><p>${emails[i].body}</p>`
-            document.querySelector("#email-view").append(element);
-            console.log("fetch complete");
+           view_email(emails[i].id);
+           console.log(emails[i].id);
           }
         })
       })
@@ -85,9 +114,75 @@ function load_mailbox(mailbox) {
         // debug with console.log
         console.log("this element's class has been changed");
      }
-    element.innerHTML = `<h6 class="sender">${emails[i].sender}</h6><p class="date">${emails[i].timestamp}</p><p>${emails[i].subject}</p>`;
+      element.innerHTML = `<h6 class="sender">${emails[i].sender}</h6><p class="date">${emails[i].timestamp}</p><p>${emails[i].subject}</p><button data-email=${emails[i].id} class='btn archive'></button>`;
       document.querySelector('#emails-view').append(element);
-      console.log("emails has been loaded");
+      element.classList.add('email');
+    console.log(`is archived ${emails[i].archived}`);
+    console.log("emails has been loaded");
   }
-  });
+  document.querySelectorAll('.archive').forEach(button =>{
+    let email_Id = parseInt(button.dataset.email);
+    button.onclick = archive;
+    console.log(`Email id is ${email_Id} testing archive button`);
+    console.log(button.dataset.emailId);
+    fetch(`emails/${email_Id}`)
+    .then(response => response.json())
+    .then(email => {
+      if(email.archived == false){
+        button.innerHTML = "Archive";
+        console.log(`this email is archived ${email.archived}`);
+      }else{
+        button.innerHTML = "Unarchive";
+        console.log(`this email is archived ${email.archived}`);
+      }
+    })
+  })
+  })
+
+}
+function view_email(email_id){
+  document.querySelector('#email-view').innerHTML = '';
+  document.querySelector('#email-view').style.display = 'block';
+  history.pushState({email: email_id, mailbox: current_mailbox}, '', `email${email_id}`);
+  console.log(email_id);
+  fetch(`emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    console.log(email.id);
+    //hide emails-view pages
+    document.querySelector('#emails-view').style.display = 'none'
+    //view an email
+    const element = document.createElement('div');
+    element.innerHTML = `<h6>From:${email.sender}</6><h6>Subject:${email.subject}</6><p>${email.timestamp}</p><p>${email.body}</p>`;
+    document.querySelector("#email-view").append(element);
+    console.log("fetch complete");
+  })
+}
+
+// archive function
+function archive(event){
+  let button = event.target;
+  let email_id = button.dataset.email;
+  fetch(`emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    if(email.archived == false){
+      fetch(`emails/${email_id}`, {
+        method : 'PUT',
+        body : JSON.stringify({
+          archived : true,
+        })
+      })
+    } else{
+      fetch(`emails/${email.id}`, {
+        method : 'PUT',
+        body : JSON.stringify({
+          archived : false,
+        })
+      })
+    }
+    button.parentElement.remove();
+    
+  })
+  
 }
